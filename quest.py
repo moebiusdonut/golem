004938fdfaa4b6fa6c31656623b728cd4ea734a1
Любинецкий <alualu@macbook-pro-lubineckij.home>
2018-11-11 09:17:29 +0100

    Moved code around, >quest help, >quest post random

--- quest.py
+++ quest.py
@@ -3,6 +3,7 @@
 import discord
 import config
 import state
+import random
 
 
 # ---------------------
@@ -16,7 +17,6 @@ class QuestPlaceData:
 
 	# Ctor ---
 	def __init__(self, p_id, p_name, p_eta, p_description=""):
-		print("QuestPlaceData: ctor (" + p_id + ", " + p_name + ", " + p_eta + ");");
 		self._id   = p_id;
 		self._name = p_name;
 		self._desc = p_description;
@@ -54,11 +54,10 @@ class QuestData:
 # ---------------------
 #   QestCommandAction
 # ---------------------
-
 class QestCommandAction:
-	_discordClient = None;
+	_discordClient  = None;
 	_discordChannel = None;
-	_gameState = None;
+	_gameState      = None;
 
 
 	# Ctor ---
@@ -75,72 +74,108 @@ class QestCommandAction:
 			return False;
 
 		action = p_argMap[config.SUBCOMMAND_KEY][0];
-		if (action == "post"):
-			pid = p_argMap.get("-pid");
-			if (pid is None):
-				await self._discordClient.send_message(self._discordChannel, "No place id specified. Use -pid.");
-				return False;
-
-			placeData = None;
-			for place in self._gameState._questPlaces:
-				if (place._id == pid):
-					placeData = place;
-					break;
-
-			if (placeData is None):
-				await self._discordClient.send_message(self._discordChannel, "Specified place id not found.");
-				return False;
-
-			questName = p_argMap.get("-n");
-			if (questName is None):
-				await self._discordClient.send_message(self._discordChannel, "No quest name specified. Use -n");
-				return False;
-
-			newQuest = QuestData(questName, placeData);
-			#globalQuests.append(newQuest);
-
-			await self.postQuestToChat(newQuest);
-			return True;
+		if action == "post":
+			return await self._process_quest_post_cmd(p_argMap);
 
 		if action == "place":
-			if len(p_argMap[config.SUBCOMMAND_KEY]) < 2:
-				msg = "_Place? This is a place, yes. Me sure quest can be somewhere in this place too_";
+			return await self._process_quest_place_cmd(p_argMap);
+
+		if action == "help":
+			return await self._process_quest_help_cmd(p_argMap);
+
+		return False;
+
+
+	# Process "quest post ..." command ---
+	async def _process_quest_post_cmd(self, p_argMap):
+		if len(p_argMap[config.SUBCOMMAND_KEY]) > 1:
+			term = p_argMap[config.SUBCOMMAND_KEY][1];
+			if term == "random":
+				name      = self._getRandomQuestName();
+				places    = self._gameState._questPlaces;
+				placeData = places[random.randint(0, len(places)-1)];
+				newQuest  = QuestData(name, placeData);
+
+				await self.postQuestToChat(newQuest);
+				return True;
+
+
+		pid = p_argMap.get("-pid");
+		if pid is None:
+			await self._discordClient.send_message(self._discordChannel, "No place id specified. Use -pid.");
+			return False;
+
+		placeData = None;
+		for place in self._gameState._questPlaces:
+			if place._id == pid:
+				placeData = place;
+				break;
+
+		if placeData is None:
+			await self._discordClient.send_message(self._discordChannel, "Specified place id not found.");
+			return False;
+
+		questName = p_argMap.get("-n");
+		if questName is None:
+			await self._discordClient.send_message(self._discordChannel, "No quest name specified. Use -n");
+			return False;
+
+		newQuest = QuestData(questName, placeData);
+		#globalQuests.append(newQuest);
+
+		await self.postQuestToChat(newQuest);
+		return True;
+
+
+	# Process "quest place ..." command ---
+	async def _process_quest_place_cmd(self, p_argMap):
+		if len(p_argMap[config.SUBCOMMAND_KEY]) < 2:
+			msg = "_Place? This is a place, yes. Me sure quest can be somewhere in this place too_";
+			await self._discordClient.send_message(self._discordChannel, msg);
+			return False;
+
+		term = p_argMap[config.SUBCOMMAND_KEY][1];
+		if term == "list":
+			# >quest place list
+			msg = "_Me remember those:\n\n";
+
+			for place in self._gameState._questPlaces:
+				msg += "**" + place._name + "** (pid: **" + place._id + "**)\n";
+
+			msg += "_";
+			await self._discordClient.send_message(self._discordChannel, msg);
+			return True;
+
+		if term == "add":
+			# >quest place add -n "Johana The Fish" -pid jfc -eta "50min+" [-desc "Some kind of description"]
+			placeName = p_argMap.get("-n");
+			placeID   = p_argMap.get("-pid");
+			placeETA  = p_argMap.get("-eta");
+			placeDesc = p_argMap.get("-desc", "");
+
+			if placeName is None or placeID is None or placeETA is None:
+				msg = "_Me not understand you, human. What is this new place you talk to me about ?_";
 				await self._discordClient.send_message(self._discordChannel, msg);
 				return False;
 
-			term = p_argMap[config.SUBCOMMAND_KEY][1];
-			if term == "list":
-				msg = "_Me remember those:\n\n";
+			newPlace = QuestPlaceData(placeID, placeName, placeETA, placeDesc);
+			self._gameState.addQuestPlace(newPlace);
 
-				for place in self._gameState._questPlaces:
-					msg += "**" + place._name + "** (pid: **" + place._id + "**)\n";
-
-				msg += "_";
-				await self._discordClient.send_message(self._discordChannel, msg);
-				return True;
-
-			if term == "add":
-				# >quest place add -n "Johana The Fish" -pid jfc -eta "50min+" [-desc "Some kind of description"]
-				placeName = p_argMap.get("-n");
-				placeID   = p_argMap.get("-pid");
-				placeETA  = p_argMap.get("-eta");
-				placeDesc = p_argMap.get("-desc", "");
-
-				if (placeName is None or placeID is None or placeETA is None):
-					msg = "_Me not understand you, human. What is this new place you talk to me about ?_";
-					await self._discordClient.send_message(self._discordChannel, msg);
-					return False;
-
-				newPlace = QuestPlaceData(placeID, placeName, placeETA, placeDesc);
-				self._gameState.addQuestPlace(newPlace);
-
-				msg = "_Ok, human. I will remember that. Golems known for good memory... i guess._";
-				await self._discordClient.send_message(self._discordChannel, msg);
-				return True;
+			msg = "_Ok, human. I will remember that. Golems known for good memory... i guess._";
+			await self._discordClient.send_message(self._discordChannel, msg);
+			return True;
 
 
-
-		return False;
+	# Process "quest place ..." command ---
+	async def _process_quest_help_cmd(self, p_argMap):
+		msg = "_Oh, me got this paper here but me not know what this means_ \n```";
+		msg += "Quests are based on places: \n\n";
+		msg += ">quest place list \n";
+		msg += '>quest place add -n "Place Name" -pid place_id -eta "Estimated time" [-desc "Optional description"]\n\n';
+		msg += "Once desired place is known you can post a quest for it:\n\n"
+		msg += '>quest post -n "New Quest Name" -pid existing_pid```';
+		await self._discordClient.send_message(self._discordChannel, msg);
+		return True;
 
 
 	# Post quest to chat ---
@@ -157,11 +192,21 @@ class QestCommandAction:
 		await self._discordClient.send_message(self._discordChannel, msg);
 
 
-	# Print usage ---
-	async def printUsage(self, p_action):
-		#msg = "Something went wrong";
-		#await p_discordClient.send_message(self._discordChannel, msg);
-		pass;
+	# Returns random quest name ---
+	def _getRandomQuestName(self):
+		pool = [\
+		"Wzooooom!",\
+		"No shame in what we do",\
+		"Food. Mouth. You dig?",\
+		"Gimme two!",\
+		"Vodka not included",\
+		"Hello, Fat",\
+		"Munch Munch",\
+		"Essentials"\
+		];
+		return pool[random.randint(0, len(pool)-1)];
 
 
 
+
+# eof
